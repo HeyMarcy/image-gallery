d23cd39f44e896546d0c82666c5ad33878776796
Marcy Montross <marcy.montross@gmail.com>
2022-12-28 16:16:41 -0600

    tweeks font size

--- components/SharedModal.tsx
+++ components/SharedModal.tsx
@@ -109,8 +109,8 @@ export default function SharedModal({
                   )}
                 </>
               )}
-              <div className='font-sm absolute bottom-0 right-4 z-50 bg-black px-2 py-1 text-white'>
-                <p className='text-xs'> {imageLabel}</p>
+              <div className='font-sm absolute bottom-0 right-4 z-50 bg-black px-4 py-2 text-white'>
+                <p className='text-sm'> {imageLabel}</p>
               </div>
 
               <div className='absolute top-0 right-0 flex items-center gap-2 p-3 text-white'>
