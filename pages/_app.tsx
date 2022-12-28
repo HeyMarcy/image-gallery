a88220d07e4bc106512c7d8c0ca19e5fba3f756c
Marcy Montross <marcy.montross@nerdery.com>
2022-12-27 21:00:31 -0600

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import type { AppProps } from 'next/app'
-import '../styles/index.css'
+import type { AppProps } from "next/app";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
+// https://res.cloudinary.com/dwfhcqaat/image/upload/v1672193750/custom-curation/Rolling_Stones_by_Gered_Mankowitz_ezx16g.jpg
